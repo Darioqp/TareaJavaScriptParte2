d373e0fe4b33cfb59277d74d969d9cda2423c571
Dario <dariokupec@gmail.com>
2021-09-22 18:44:07 -0300

    Se realizó el ejercicio 3

--- ejercicios.js
+++ ejercicios.js
@@ -17,10 +17,13 @@ mensaje.addEventListener('input', function(e){
     contador.innerHTML = `${cantidadActual}/${cantidadMaxima}`
 })
 
-
 // Ejercicio 3
 
+var alumnos = [{nombre: 'Juan Gomez', nota: 7}, {nombre: 'Pedro Rodriguez', nota: 4}, {nombre: 'Roxana García', nota: 8}, {nombre: 'Luciano Lopez', nota: 5}, {nombre: 'Fernanda Gimenez', nota: 6}, {nombre: 'Florencia Martinez', nota: 10}, {nombre: 'Raul Sanchez', nota: 7}, {nombre: 'Sandra Figueroa', nota: 8}]
 
+var alumnosAprobados = alumnos.filter(alumno => alumno.nota >= 7)
+
+console.log(alumnosAprobados)
 
 
 
